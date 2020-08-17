16b196126e794b32cf44656d2b9c9df478b01c29
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-17 19:44:09 +0900

    新規サークル作成機能を実装

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -8,11 +8,20 @@ class CirclesController < ApplicationController
 	end
 
 	def create
+		@circle = Circle.new(circle_params)
+		binding.pry
+		if @circle.save
+			flash[:success] = "Create circle"
+			redirect_to root_path
+		else
+			flash[:danger] = "Failed create"
+			render :new
+		end
 	end
 
 	private
 
 	def circle_params
-		params.require(:circle).permit(:name, :description, :genre_id, :activity_id, :age_range_id, :prefecture_id).merge(user_id: current_user.id)
+		params.require(:circle).permit(:name, :description, :genre_id, :activity_id, :age_range_id, :prefecture_id).merge(user_ids: current_user.id)
 	end
 end
