9f8102fd4436a06af2834ccc50347986a684e4ef
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-17 12:43:46 +0900

    circleモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_023635) do
+ActiveRecord::Schema.define(version: 2020_08_17_034135) do
+
+  create_table "circles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "genre_id", null: false
+    t.integer "activity_id", null: false
+    t.integer "age_range_id", null: false
+    t.integer "prefecture_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_circles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "circle_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["circle_id"], name: "index_user_circles_on_circle_id"
+    t.index ["user_id"], name: "index_user_circles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +45,6 @@ ActiveRecord::Schema.define(version: 2020_08_16_023635) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_circles", "circles"
+  add_foreign_key "user_circles", "users"
 end
