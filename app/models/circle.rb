99c4be7ce4e548a5d1cc0174383a61e858840d76
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-17 12:46:42 +0900

    アソシエーションの定義

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,2 +1,4 @@
 class Circle < ApplicationRecord
+	has_many :user_circles
+	has_many :users, through: :user_circles
 end
--- app/models/user_circle.rb
+++ app/models/user_circle.rb
@@ -1,2 +1,4 @@
 class UserCircle < ApplicationRecord
+	belongs_to :user
+	belongs_to :circle
 end
