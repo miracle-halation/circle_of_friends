2fd0c73281ea95cb45a9ac7370d3ee088e9b2231
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-17 14:57:43 +0900

    ストロングパラメータの実装

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -9,4 +9,10 @@ class CirclesController < ApplicationController
 
 	def create
 	end
+
+	private
+
+	def circle_params
+		params.require(:circle).permit(:name, :description, :genre_id, :activity_id, :age_range_id, :prefecture_id).merge(user_id: current_user.id)
+	end
 end
