a5ed52b21bd7f91d306a757da69ddd31c8adbf50
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-17 12:50:42 +0900

    バリデーションの設定

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,4 +1,13 @@
 class Circle < ApplicationRecord
 	has_many :user_circles
 	has_many :users, through: :user_circles
+
+	with_options presence: true do
+		validates :name
+		validates	:description, length: { maximum: 1000 }
+		validates :genre_id, numericality: { other_than: 1 }
+		validates :activity_id, numericality: { other_than: 1 }
+		validates :age_range_id, numericality: { other_than: 1 }
+		validates :prefecture_id, numericality: { other_than: 1 }
+	end
 end
