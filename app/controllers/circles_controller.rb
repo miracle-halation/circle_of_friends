d67097f84e2dfe442052c41d581a58cc5d05e744
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-18 11:17:22 +0900

    leaderが新規立ち上げの時に保存される

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -22,6 +22,6 @@ class CirclesController < ApplicationController
 	private
 
 	def circle_params
-		params.require(:circle).permit(:name, :description, :genre_id, :activity_id, :age_range_id, :prefecture_id).merge(user_ids: current_user.id)
+		params.require(:circle).permit(:name, :description, :genre_id, :activity_id, :age_range_id, :prefecture_id, :leader).merge(user_ids: current_user.id)
 	end
 end
