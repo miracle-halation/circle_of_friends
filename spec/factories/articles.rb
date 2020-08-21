685bcb79cf88bc2514af7ea3d14c9b6a64307071
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-21 10:27:06 +0900

    モデルの単体テストを作成

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :article do
-    
+    title {Faker::Lorem.sentence}
+    content {Faker::Lorem.sentence}
+    image {Rack::Test::UploadedFile.new('public/images/test_image.jpg')}
+    association :user
+    association :circle
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "新規作成"  do
+    let!(:article){FactoryBot.build(:article)}
+
+    it "全ての情報が正しいと保存できる" do
+      expect(article).to be_valid
+    end
+    it "titleが空だと保存できない" do
+      article.title = nil
+      article.valid?
+      expect(article.errors.full_messages).to include("Title can't be blank")
+    end
+    it "contentが空だと保存できない" do
+      article.content = nil
+      article.valid?
+      expect(article.errors.full_messages).to include("Content can't be blank")
+    end
+    it "imageが空でも保存できる" do
+      article.image = nil
+      expect(article).to be_valid
+    end
+    it "userと紐づいていないと保存できない" do
+      article.user = nil
+      article.valid?
+      expect(article.errors.full_messages).to include("User must exist")
+    end
+    it "circleと紐づいていないと保存できない" do
+      article.circle = nil
+      article.valid?
+      expect(article.errors.full_messages).to include("Circle must exist")
+    end
+  end
 end
