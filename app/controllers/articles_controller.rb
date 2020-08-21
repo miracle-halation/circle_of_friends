f92e1477465547a7485edc84611c55ddb00b1b7c
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-21 14:39:17 +0900

    アクセス制限の継ぎ足し

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :sign_in_user?, only: [:new, :edit]
+  before_action :sign_in_user?
   before_action :set_circle
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   before_action :circle_user?, only: [:new, :edit]
