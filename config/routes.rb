58fdf14362aff0cbdf9c266376e84313a20f1b3d
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-21 14:18:12 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :circles do
     get "joing"
     get "withdrawal"
-    resources :articles, only: [:new, :show, :create]
+    resources :articles, except: [:index]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
