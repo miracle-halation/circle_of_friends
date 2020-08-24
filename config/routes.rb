405af11e574f97255b48073c65b539b567ba96ce
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-24 11:07:35 +0900

    eventsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     get "joing"
     get "withdrawal"
     resources :articles, except: [:index]
+    resources :events, only: [:new, :show, :create, :edit, :update]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
