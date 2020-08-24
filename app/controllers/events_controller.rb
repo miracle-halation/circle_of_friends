fcc7365ed42ef3f238e78d7604c6a3945f4b6b4c
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-24 12:47:16 +0900

    イベント新規作成機能を実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,12 @@ class EventsController < ApplicationController
 	end
 
 	def create
+		@event = @circle.events.new(event_params)
+		if @event.save
+			redirect_to @circle
+		else
+			render :new
+		end
 	end
 
 	def show
@@ -20,6 +26,10 @@ class EventsController < ApplicationController
 	end
 
 	private
+
+	def event_params
+		params.require(:event).permit(:title, :body, :start_time, :end_time, :circle_id, user_ids:[])
+	end
 
 	def set_circle
     @circle = Circle.find(params[:circle_id])
