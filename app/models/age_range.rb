8b36e7c1c6ed6daf1767de0544dff88f059ef2ff
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-26 15:41:38 +0900

    ActiveHashの値を追加＆変更

--- app/models/age_range.rb
+++ app/models/age_range.rb
@@ -1,10 +1,10 @@
 class AgeRange < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '20代が多い' },
-    { id: 3, name: '30代が多い' },
-    { id: 4, name: '40代が多い' },
-    { id: 5, name: '50代が多い' },
-    { id: 6, name: '60代が多い' }
+    { id: 2, name: '20代~30代' },
+    { id: 3, name: '30代~40代' },
+    { id: 4, name: '40代~50代' },
+    { id: 5, name: '50代~60代' },
+    { id: 6, name: '60代~' }
   ]
 end
