098453a705c1882317f190a3b953fc5d52155b02
miracle-halation <miraclehalation.9710@gmail.com>
2020-08-27 16:02:52 +0900

    rubocop指摘箇所手動修正

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -75,7 +75,8 @@ class CirclesController < ApplicationController
 
   def circle_params
     params.require(:circle)
-          .permit(:name, :phrase, :description, :genre_id, :activity, :age_range, :prefecture_id, :city, :leader, :image, user_ids: [])
+          .permit(:name, :phrase, :description, :genre_id, :activity, :age_range, :prefecture_id, :city,
+                  :leader, :image, user_ids: [])
           .merge(leader_user: current_user)
   end
 
